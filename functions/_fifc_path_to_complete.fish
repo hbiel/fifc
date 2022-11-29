f4f8d16a2fb20fe58de439771f88128986a9f7a0
Helge Biel <18981810+hbiel@users.noreply.github.com>
2022-11-29 19:23:21 +0000

    fix: expand ~ to $HOME in path completion

--- functions/_fifc_path_to_complete.fish
+++ functions/_fifc_path_to_complete.fish
@@ -1,6 +1,6 @@
 function _fifc_path_to_complete
     set -l token (string unescape -- $fifc_token)
-    if string match --regex --quiet -- '.*(\w|\.|/)+$' "$token"
+    if string match --regex --quiet -- '.*(~|\w|\.|/)+$' "$token"
         _fifc_expand_tilde "$token"
     else
         echo {$PWD}/
