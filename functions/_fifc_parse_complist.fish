aa973e36304f99f9238498583035ad2c4ff1a327
Helge Biel <18981810+hbiel@users.noreply.github.com>
2023-01-16 20:50:34 +0100

    feature: set tabstop according to the longest first column

--- functions/_fifc_parse_complist.fish
+++ functions/_fifc_parse_complist.fish
@@ -1,4 +1,9 @@
-function _fifc_parse_complist -d "Extract the first column of fish completion list"
+function _fifc_parse_complist -d "Parse fish completion list and set the tabstop accordingly"
+    set tabstop (math (cat $_fifc_complist_path | awk '{print $1}' | wc -L) + 4)
+    if test $tabstop -gt $_fifc_tabstop
+        set _fifc_tabstop $tabstop
+    end
+
     cat $_fifc_complist_path \
         | string unescape \
         | uniq
